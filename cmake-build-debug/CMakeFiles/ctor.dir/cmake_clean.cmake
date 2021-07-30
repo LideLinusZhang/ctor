d23628d2abdd2a56cfadb128d8094e184ea5b932
Lide Linus Zhang <39998175+LideLinusZhang@users.noreply.github.com>
2021-07-30 17:00:55 +0800

    CMake files

---
+++ cmake-build-debug/CMakeFiles/ctor.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ctor.dir/fairDice.cc.obj"
+  "CMakeFiles/ctor.dir/view.cc.obj"
+  "ctor.exe"
+  "ctor.exe.manifest"
+  "ctor.pdb"
+  "libctor.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ctor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ctor.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ctor.
+# This may be replaced when dependencies are built.
