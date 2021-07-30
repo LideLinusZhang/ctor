ece0e0909c815987864f10a4a4e3422ea40bef73
Lide Linus Zhang <39998175+LideLinusZhang@users.noreply.github.com>
2021-07-30 19:36:47 +0800

    Partial implementation on Game.

--- game.h
+++ game.h
@@ -3,29 +3,32 @@
 
 #include <vector>
 #include <memory>
+#include <string>
+
+#define TOTAL_PLAYERS 4
 
 class View;
 class Player;
 class Board;
 class BoardFactory;
-class FileMode;
+class Dice;
 
 class Game
 {
     std::shared_ptr<View> view;
     std::shared_ptr<Board> gameBoard;
-    Dice loaded;
-    Dice fair;
-    BoardFactory& boardFactory;
+    std::unique_ptr<Dice> loaded;
+    std::unique_ptr<Dice> fair;
+    std::shared_ptr<BoardFactory> boardFactory;
     std::vector<std::shared_ptr<Player>> players;
 
     void save();
-    void init();
-    void init(std::string fileName);
+    void initPlayers(); // Initialize players to their default state.
+    void read(std::string& fileName); // Read the entire class from file.
+    Game(); // Only initiate dices.
 public:
-    explicit Game(std::string fileName);
-    explicit Game(BoardFactory& factory);
-    Game(FileMode& factory, std::string fileName);
+    explicit Game(std::string& fileName);
+    explicit Game(std::shared_ptr<BoardFactory> factory);
 
     void play();
 };
