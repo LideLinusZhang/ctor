9d424dcd15e03fcc84e57a1bc3d82d6cccdc7e96
Lide Linus Zhang <39998175+LideLinusZhang@users.noreply.github.com>
2021-08-01 00:12:17 +0800

    Partial implementation of Game.

--- game.cc
+++ game.cc
@@ -1,26 +1,32 @@
 #include <utility>
+#include <sstream>
 #include "fairDice.h"
 #include "loadedDice.h"
 #include "player.h"
 #include "game.h"
 #include "boardFactory.h"
+#include "view.h"
+#include "board.h"
+#include "tile.h"
+#include "vertex.h"
+#include "edge.h"
 
 using namespace std;
 
-Game::Game()
+Game::Game(istream &input) : input{input}
 {
-    random_device rd;
+    input.exceptions(ios::eofbit | ios::failbit);
 
     loaded = make_unique<LoadedDice>(view.get());
-    fair = make_unique<FairDice>(rd());
+    fair = make_unique<FairDice>();
 }
 
-Game::Game(string &fileName) : Game()
+Game::Game(const string &fileName, istream &input) : Game(input)
 {
     read(fileName);
 }
 
-Game::Game(shared_ptr<BoardFactory> factory) : Game()
+Game::Game(shared_ptr<BoardFactory> factory, istream &input) : Game(input)
 {
     boardFactory = move(factory);
     gameBoard = boardFactory->createBoard(view.get());
@@ -29,10 +35,216 @@ Game::Game(shared_ptr<BoardFactory> factory) : Game()
 
 void Game::initPlayers()
 {
-    for (int i = 0; i < TOTAL_PLAYERS; i++)
+    for (int i = 0; i < totalPlayers; i++)
     {
         auto player = make_shared<Player>(view.get(), gameBoard.get(), static_cast<Color>(i));
         players.emplace_back(move(player));
+    }
+}
+
+void Game::play()
+{
+    for (;; currentPlayerIndex = (currentPlayerIndex + 1) % 4)
+    {
+        Player *currentPlayer = players[currentPlayerIndex].get();
+
+        try
+        {
+            beginTurn(currentPlayer);
+            duringTurn(currentPlayer);
+        }
+        catch (ios_base::failure &)
+        {
+            save();
+            return;
+        }
+
+        // TODO
+    }
+}
+
+void Game::beginTurn(Player *player)
+{
+    ostringstream message;
+
+    message << "Builder " << toString(player->getColor()) << "\'s turn." << endl;
+    view->printMessage(message.str());
+
+    int rollResult;
+    try
+    { rollResult = roll(); }
+    catch (ios_base::failure &)
+    { throw; }
+
+    if (rollResult == 7)
+        moveGeese(player);
+    else
+        obtainResources(rollResult);
+}
+
+void Game::obtainResources(int rollResult)
+{
+    vector<map<ResourceType, int>> resourcesSnapshot;
+
+    for (int i = 0; i < totalPlayers; i++)
+    {
+        resourcesSnapshot.emplace_back(map<ResourceType, int>());
+        for (int j = 0; j < resourceTypeCount; j++)
+        {
+            auto type = static_cast<ResourceType>(j);
+            resourcesSnapshot[i].insert({type, players[i]->getResource(type)});
+        }
+    }
+
+    for (int i = minTileIndex; i <= maxTileIndex; i++)
+    {
+        auto tile = gameBoard->getTile(i);
+
+        if (tile->getValue() == rollResult)
+            tile->obtainResource();
+    }
+
+    bool anyPlayerGainedResource = false;
+    ostringstream message;
+
+    for (int i = 0; i < totalPlayers; i++)
+    {
+        bool thisPlayerGainedResource = false;
+        for (int j = 0; j < resourceTypeCount; j++)
+        {
+            auto type = static_cast<ResourceType>(j);
+            // If a player gains resources, his current resource number
+            // will not equal to the resource number in the snapshot.
+            if (players[i]->getResource(type) != resourcesSnapshot[i][type])
+            {
+                int numGained = resourcesSnapshot[i][type] - players[i]->getResource(type);
+                if (!thisPlayerGainedResource)
+                {
+                    thisPlayerGainedResource = true;
+                    anyPlayerGainedResource = true;
+                    auto color = static_cast<Color>(i);
+                    message << "Builder " << color << " gained:" << endl;
+                }
+
+                message << numGained << " " << toStringAllCaps(type) << endl;
+            }
+        }
+    }
+
+    if (!anyPlayerGainedResource)
+        message << "No builders gained resources." << endl;
+
+    view->printMessage(message.str());
+}
+
+int Game::roll()
+{
+    string cmd;
+    shared_ptr<Dice> dice(nullptr);
+    int rollResult;
+
+    while (true)
+    {
+        try
+        { input >> cmd; }
+        catch (ios_base::failure &)
+        { throw; }
+
+        if (cmd == "roll")
+        {
+            if (dice == nullptr)
+                view->printError(ErrorType::InvalidRoll);
+            else
+            {
+                try
+                { rollResult = dice->roll(); }
+                catch (ios_base::failure &)
+                { throw; }
+                break;
+            }
+        }
+        else if (cmd == "load")
+            dice = loaded;
+        else if (cmd == "fair")
+            dice = fair;
+        else
+            view->printError(ErrorType::InvalidCommand);
+    }
+
+    return rollResult;
+}
+
+void Game::duringTurn(Player *player)
+{
+    string cmd;
+    string colorStr, takeStr, giveStr;
+    string fileName;
+    int index;
+
+    while (true)
+    {
+        try
+        { input >> cmd; }
+        catch (ios_base::failure &)
+        { throw; }
+
+        if (cmd == "board")
+            view->printBoard(gameBoard.get());
+        else if (cmd == "status")
+        {
+            for (auto &p : players)
+                p->printStatus();
+        }
+        else if (cmd == "residences")
+            player->printResidences();
+        else if (cmd == "build-road" || cmd == "build-res" || cmd == "improve")
+        {
+            try
+            { input >> index; }
+            catch (ios_base::failure &)
+            {
+                view->printError(ErrorType::InvalidOperation);
+                continue;
+            }
+
+            if (cmd == "build-road")
+                gameBoard->getEdge(index)->buildRoad(player);
+            else if (cmd == "build-res")
+                gameBoard->getVertex(index)->build(player);
+            else
+                gameBoard->getVertex(index)->improve(player);
+        }
+        else if (cmd == "trade")
+        {
+            try
+            { input >> colorStr >> takeStr >> giveStr; }
+            catch (ios_base::failure &)
+            {
+                view->printError(ErrorType::InvalidOperation);
+                continue;
+            }
+            // TODO
+        }
+        else if (cmd == "next")
+            return;
+        else if (cmd == "save")
+        {
+            try
+            { input >> fileName; }
+            catch (ios_base::failure &)
+            {
+                view->printError(ErrorType::InvalidOperation);
+                continue;
+            }
+
+            save(fileName);
+        }
+        else if (cmd == "help")
+        {
+            // TODO
+        }
+        else
+            view->printError(ErrorType::InvalidCommand);
     }
 }
 
--- game.h
+++ game.h
@@ -4,31 +4,50 @@
 #include <vector>
 #include <memory>
 #include <string>
-
-#define TOTAL_PLAYERS 4
+#include <iostream>
 
 class View;
+
 class Player;
+
 class Board;
+
 class BoardFactory;
+
 class Dice;
+
+const int totalPlayers = 4;
 
 class Game
 {
+    // Utilities
     std::shared_ptr<View> view;
-    std::shared_ptr<Board> gameBoard;
-    std::unique_ptr<Dice> loaded;
-    std::unique_ptr<Dice> fair;
     std::shared_ptr<BoardFactory> boardFactory;
-    std::vector<std::shared_ptr<Player>> players;
+    std::istream &input; // Input source.
 
-    void save();
+    // Dices
+    std::shared_ptr<Dice> loaded;
+    std::shared_ptr<Dice> fair;
+
+    // Game components
+    int currentPlayerIndex = -1;
+    std::vector<std::shared_ptr<Player>> players;
+    std::shared_ptr<Board> gameBoard;
+
+    void beginTurn(Player* player);
+    int roll();
+    void obtainResources(int rollResult);
+    void moveGeese(Player* player);
+
+    void duringTurn(Player* player);
+
+    void save(const std::string &fileName = "backup.sv");
     void initPlayers(); // Initialize players to their default state.
-    void read(std::string& fileName); // Read the entire class from file.
-    Game(); // Only initiate dices.
+    void read(const std::string &fileName); // Read the entire class from file.
+    explicit Game(std::istream &input); // Only initiate dices.
 public:
-    explicit Game(std::string& fileName);
-    explicit Game(std::shared_ptr<BoardFactory> factory);
+    explicit Game(const std::string &fileName, std::istream &input = std::cin);
+    explicit Game(std::shared_ptr<BoardFactory> factory, std::istream &input = std::cin);
 
     void play();
 };
