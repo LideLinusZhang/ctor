5eeb4c40db044bc13502bb4c3fdce4532fda779c
9cTY <yeyiheng666666@yeah.net>
2021-07-31 17:14:28 +0800

    Finished Geese.

---
+++ geese.cc
@@ -0,0 +1,17 @@
+#include "geese.h"
+
+Geese::Geese(int position):position(position){
+
+}
+
+bool Geese::tryMoveTo(int tileIndex){
+    if(tileIndex>=0 && tileIndex<=18 && tileIndex!=position){
+        position = tileIndex;
+        return true;
+    }
+    return false;
+}
+
+inline int Geese::getPosition() const{
+    return position;
+}
