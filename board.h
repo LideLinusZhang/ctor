65c5f72cb96420bee662aeb4e424086852a17bc5
MingxiaoZhang <86276506+MingxiaoZhang@users.noreply.github.com>
2021-07-31 23:59:02 +0800

    Update board.h

--- board.h
+++ board.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <memory>
+#include "geese.h"
 
 class View;
 class Tile;
@@ -18,7 +19,7 @@ class Board
     Geese geese;
 public:
     // Construct by copying pre-constructed vectors.
-    Board(View* view, vector<std::shared_ptr<Tile>> tiles, std::vector<std::shared_ptr<Edge>> edges,
+    Board(View* view, std::vector<std::shared_ptr<Tile>> tiles, std::vector<std::shared_ptr<Edge>> edges,
           std::vector<std::shared_ptr<Vertex>> vertices, int geeseInitialPosition);
 
     Tile* getTile(int index) const;
