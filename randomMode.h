468a61480d435c9a6ae503b0490c6a2175753d90
MingxiaoZhang <86276506+MingxiaoZhang@users.noreply.github.com>
2021-08-01 14:12:03 +0800

    Update randomMode.h
    Updated with Random Engine

--- randomMode.h
+++ randomMode.h
@@ -2,26 +2,19 @@
 #define CTOR_RANDOMMODE_H
 
 #include <random>
+#include <algorithm>
 #include "boardFactory.h"
 
 class View;
-
-// use the static Random class for random number generation.
 
 class RandomMode : public BoardFactory
 {
 public:
     RandomMode();
     std::shared_ptr<Board> createBoard(View* view) override;
-
-private: 
-    std::map<ResourceType, int> resources = {{ResourceType::Wifi, 3}, 
-                                                {ResourceType::Heat, 3},
-                                                {ResourceType::Brick, 4},
-                                                {ResourceType::Energy, 4},
-                                                {ResourceType::Glass, 4},
-                                                {ResourceType::Parl, 1},
-                                                }
+private:
+    std::vector<ResourceType> shuffleResource();
+    std::vector<int> shuffleValues(std::vector<ResourceType> resources);
 };
 
 #endif //CTOR_RANDOMMODE_H
