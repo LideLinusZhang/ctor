ccd763acdb04b3a34b9c9c4769e398862ae76572
Lide Linus Zhang <39998175+LideLinusZhang@users.noreply.github.com>
2021-08-01 15:15:35 +0800

    Added missing method.

--- tile.h
+++ tile.h
@@ -2,9 +2,12 @@
 #define CTOR_TILE_H
 
 #include "resourceType.h"
+#include "color.h"
 #include <vector>
 
 class Board;
+
+class Player;
 
 class Tile
 {
@@ -18,6 +21,9 @@ public:
     void obtainResource(); // Add resources to all players who have building around this tile.
     ResourceType getType() const;
     int getValue() const;
+    // Return a list that contains the players that have residences on this tile.
+    // The list should be duplicate-free and sorted.
+    std::vector<Player*> getResidenceOwners() const;
 };
 
 #endif //CTOR_TILE_H
