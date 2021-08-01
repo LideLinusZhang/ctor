5513930cb98797f6bb2aa4bd39d42730f2748025
Lide Linus Zhang <39998175+LideLinusZhang@users.noreply.github.com>
2021-08-01 13:56:34 +0800

    Revert "Update randomMode.h"
    This reverts commit b06ffd8eb7543b2319ba893405a402574e5879ac.

--- randomMode.h
+++ randomMode.h
@@ -2,20 +2,26 @@
 #define CTOR_RANDOMMODE_H
 
 #include <random>
-#include <algorithm>
 #include "boardFactory.h"
 
 class View;
 
+// use the static Random class for random number generation.
+
 class RandomMode : public BoardFactory
 {
-    const int seed;
 public:
-    explicit RandomMode(const int seed);
+    RandomMode();
     std::shared_ptr<Board> createBoard(View* view) override;
-private:
-    std::vector<ResourceType> shuffleResource();
-    std::vector<int> shuffleValues(std::vector<ResourceType> resources);
+
+private: 
+    std::map<ResourceType, int> resources = {{ResourceType::Wifi, 3}, 
+                                                {ResourceType::Heat, 3},
+                                                {ResourceType::Brick, 4},
+                                                {ResourceType::Energy, 4},
+                                                {ResourceType::Glass, 4},
+                                                {ResourceType::Parl, 1},
+                                                }
 };
 
 #endif //CTOR_RANDOMMODE_H
