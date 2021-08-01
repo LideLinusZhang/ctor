64efcde82ece266dabfdad720e450edbc1931af3
MingxiaoZhang <86276506+MingxiaoZhang@users.noreply.github.com>
2021-08-01 13:23:40 +0800

    Update fileMode.cc

--- fileMode.cc
+++ fileMode.cc
@@ -2,6 +2,7 @@
 
 FileMode::FileMode(std::istream& file) : file{file} {
 }
+
 std::shared_ptr<Board> FileMode::createBoard(View* view) {
     std::vector<std::shared_ptr<Vertex>> vertices = createVertices(view);
     std::vector<std::shared_ptr<Edge>> edges = createEdges(view, &vertices);
@@ -24,12 +25,47 @@ std::shared_ptr<Board> FileMode::createBoard(View* view) {
             case 3:
                 type = ResourceType::Heat;
             case 4:
-                type = ResourceType::Wifi;
+                type = ResourceType::WiFi;
             case 5:
                 type = ResourceType::Park;
         }
         tiles.push_back(std::make_shared<Tile>(this, vertexTiles[i], type, value));
     }
-    int geesePosition = boardFactory::geeseParkIndex(view, tiles);
-    return make_shared<Board>(view, tiles, edges, vertices, geesePosition);
+    int geesePosition = BoardFactory::getParkIndex(view, tiles);
+    return std::make_shared<Board>(view, tiles, edges, vertices, geesePosition);
+}#include "fileMode.h"
+
+FileMode::FileMode(std::istream& file) : file{file} {
+}
+
+std::shared_ptr<Board> FileMode::createBoard(View* view) {
+    std::vector<std::shared_ptr<Vertex>> vertices = createVertices(view);
+    std::vector<std::shared_ptr<Edge>> edges = createEdges(view, &vertices);
+    std::vector<std::vector<int>> vertexTiles = createTileVertices();
+    std::vector<std::shared_ptr<Tile>> tiles;
+    int resource = 0;
+    int value = 0;
+    ResourceType type;
+    for (int i = 0; i < TOTAL_TILES; i++) {
+        file >> resource;
+        file >> value;
+        ResourceType type;
+        switch (resource) {
+            case 0:
+                type = ResourceType::Brick;
+            case 1:
+                type = ResourceType::Energy;
+            case 2:
+                type = ResourceType::Glass;
+            case 3:
+                type = ResourceType::Heat;
+            case 4:
+                type = ResourceType::WiFi;
+            case 5:
+                type = ResourceType::Park;
+        }
+        tiles.push_back(std::make_shared<Tile>(this, vertexTiles[i], type, value));
+    }
+    int geesePosition = BoardFactory::getParkIndex(view, tiles);
+    return std::make_shared<Board>(view, tiles, edges, vertices, geesePosition);
 }
