574decd970fa00362716ae4c6dbf87d8720ce412
MingxiaoZhang <86276506+MingxiaoZhang@users.noreply.github.com>
2021-08-03 13:27:27 +0800

    Update board.cc

--- models/board.cc
+++ models/board.cc
@@ -8,9 +8,8 @@
 
 using namespace std;
 
-Board::Board(View* view, std::vector<std::shared_ptr<Tile>> tiles,
-            int geeseInitialPosition)
-            : view{view}, tiles{std::move(tiles)}, geese{geeseInitialPosition} {
+Board::Board(View* view, std::vector<std::shared_ptr<Tile>> tiles)
+            : view{view}, tiles{std::move(tiles)} {
     for(auto i : vertexAdjacentEdges) {
         vertices.push_back(std::make_shared<Vertex>(view, i));
     }
@@ -18,6 +17,11 @@ Board::Board(View* view, std::vector<std::shared_ptr<Tile>> tiles,
         std::vector<int> v(edgeAdjacentVertices[i], edgeAdjacentVertices[i] + 2);
         edges.push_back(std::make_shared<Edge>(view, v));
     }
+    for (int i = 0; i < tiles.size(); i++) {
+        if (tiles[i]->getType() == Park) {
+            geese = std::make_shared<Geese>(i);
+        }
+    }      
     setBoard();
 }
 
