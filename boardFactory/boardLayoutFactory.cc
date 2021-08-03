1df9a099de141cf836c065abae65abbcfa24b20f
MingxiaoZhang <86276506+MingxiaoZhang@users.noreply.github.com>
2021-08-03 13:27:54 +0800

    Update boardLayoutFactory.cc

--- boardFactory/boardLayoutFactory.cc
+++ boardFactory/boardLayoutFactory.cc
@@ -1,10 +1,2 @@
 #include "boardLayoutFactory.h"
 
-int BoardLayoutFactory::getParkIndex(View *view, std::vector<std::shared_ptr<Tile>> tiles) {
-    for (auto it : tiles) {
-        if (it->getType() == Park) {
-            return it->getIndex();
-        }
-    }
-    return 0;
-}
