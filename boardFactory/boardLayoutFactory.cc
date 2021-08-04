a025e788b6336b02af240de5bc56ccd29ca9d22a
MingxiaoZhang <86276506+MingxiaoZhang@users.noreply.github.com>
2021-08-04 10:20:04 +0800

    Update boardLayoutFactory.cc

--- boardFactory/boardLayoutFactory.cc
+++ boardFactory/boardLayoutFactory.cc
@@ -2,5 +2,5 @@
 
 void BoardLayoutFactory::setLayout(Board *board, std::vector<std::shared_ptr<Tile>> &tiles)
 {
-    std::move(tiles.begin(), tiles.end(), board->tiles);
+     std::copy(tiles.begin(), tiles.end(), std::back_inserter(board->tiles));
 }
