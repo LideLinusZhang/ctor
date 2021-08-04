7a564a17b799b590da4a61be2a287992c42b6e94
9cTY <yeyiheng666666@yeah.net>
2021-08-04 15:54:42 +0800

    Fix some bugs.

--- edge.cc
+++ edge.cc
@@ -1,15 +1,15 @@
 #include "edge.h"
-#include "../view/view.h"
+#include "view.h"
 #include "board.h"
 #include <utility>
 #include "player.h"
+#include "vertex.h"
 
 using namespace std;
 
 // Construct as an ordinary edge
-Edge::Edge(View *view, Board *board, vector<int> adjacentEdgeIndices, vector<int> adjacentVertexIndices)
-        : view(view), board(board), adjacentEdgeIndices(std::move(adjacentEdgeIndices)),
-          adjacentVertexIndices(std::move(adjacentVertexIndices))
+Edge::Edge(View *view, Board *board, vector<int> adjacentVertexIndices)
+        : view(view), board(board), adjacentVertexIndices(std::move(adjacentVertexIndices))
 {
     road = false;
     owner = nullptr;
@@ -28,12 +28,28 @@ void Edge::buildRoad(Player *p)
     for(size_t i = 0; adjacentVertexIndices.size(); i++)
     {
         Vertex *v = board->getVertex(adjacentVertexIndices[i]);
-        if(v->getOwner() == owner)
+        if(v->getOwner() == p)
         {
             is_link = true;
             break;
         }
+        std::vector<int> vEdgeIndices = v->getEdgeIndices();
+        for(size_t k=0;k<vEdgeIndices.size();k++)
+        {
+            Edge *e = board->getEdge(vEdgeIndices[k]);
+            if(e != this && e->getOwner() == p)
+            {
+                is_link = true;
+                break;
+            }
+        }
+        if(is_link)
+        {
+            break;
+        }
+
     }
+
     if (!is_link)
     {
         view->printError(ErrorType::InvalidBuildOrImprove);
@@ -51,15 +67,14 @@ void Edge::buildRoad(Player *p)
         p->setResource(ResourceType::WiFi, --wifi_num);
         p->setResource(ResourceType::Heat, --heat_num);
     }
-
 }
 
-inline bool Edge::isRoad() const
+bool Edge::isRoad() const
 {
     return road;
 }
 
-inline Player *Edge::getOwner() const
+Player *Edge::getOwner() const
 {
     return owner;
 }
