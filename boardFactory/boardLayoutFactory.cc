bab15cf6df5207e1b7924ebe96fa68d1113a2d89
Lide Linus Zhang <39998175+LideLinusZhang@users.noreply.github.com>
2021-08-04 10:41:55 +0800

    Fixed an issue with moving elements of a vector.

--- boardFactory/boardLayoutFactory.cc
+++ boardFactory/boardLayoutFactory.cc
@@ -2,5 +2,6 @@
 
 void BoardLayoutFactory::setLayout(Board *board, std::vector<std::shared_ptr<Tile>> &tiles)
 {
-    std::move(tiles.begin(), tiles.end(), board->tiles);
+    std::move(tiles.begin(), tiles.end(), std::back_inserter(board->tiles));
+    tiles.clear();
 }
